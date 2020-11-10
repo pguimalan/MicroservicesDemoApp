fb618c3ab549cbb8f77dddc20bf01bb377be4b36
LAPTOP-VPET5ADF\pguim <pguimalan@gmail.com>
2020-11-10 16:39:52 +0000

    Initial Commit for project Order.Api

---
+++ MicroservicesDemoApp/src/Ordering.Api/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
